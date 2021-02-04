30ab9ae77fa4bd01af480ff1cc0b2d0a2144a776
an0malous <anomalous58@gmail.com>
2021-02-05 00:18:03 +0900

    added abstract class and refactored numers linked & char classes

--- sorting-app/build/index.js
+++ sorting-app/build/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var Sorter_js_1 = require("./Sorter.js");
-var CharactersCollection_js_1 = require("./CharactersCollection.js");
-var charactersCollection = new CharactersCollection_js_1.CharactersCollection('asdasdXXba');
-var sorter = new Sorter_js_1.Sorter(charactersCollection);
-sorter.sort();
-console.log(charactersCollection.data);
+var LinkedList_js_1 = require("./LinkedList.js");
+var linkedList = new LinkedList_js_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
--- sorting-app/src/index.ts
+++ sorting-app/src/index.ts
@@ -1,9 +1,12 @@
 import { Sorter } from './Sorter.js'
 import { NumbersCollection } from './NumbersCollection.js';
 import { CharactersCollection } from './CharactersCollection.js';
+import { LinkedList } from './LinkedList.js';
 
  
-const charactersCollection = new CharactersCollection('asdasdXXba');
-const sorter = new Sorter(charactersCollection)
-sorter.sort()
-console.log(charactersCollection.data)
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3)
+linkedList.add(4);
+
