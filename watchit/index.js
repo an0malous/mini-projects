e1f0b6f9f8ab9fc34dca7123d60fcfcfdfb4c7db
an0malous <anomalous58@gmail.com>
2021-02-08 12:29:10 +0900

    added in basic logic and debounce

--- watchit/index.js
+++ watchit/index.js
@@ -1,3 +1,46 @@
 #!/usr/bin/env node
+const chokidar = require('chokidar');
+const prog = require('caporal');
+const fs = require('fs');
+const { spawn } = require('child_process');
 
-console.log("I was executed")
+const debounce = (func, delay = 1000) => {
+   let timeoutId;
+   return (...args) => {
+       if(timeoutId) {
+           clearTimeout(timeoutId);
+       }
+       timeoutId = setTimeout(() => {
+           func.apply(null, args);
+       }, delay)
+   };
+};
+
+prog
+   .version('0.0.1')
+	.argument('[filename]', 'Name of a file or execute')
+	.action(async ({ filename }) => {
+		const name = filename || 'index.js';
+
+		try {
+			await fs.promises.access(name);
+		} catch (err) {
+			throw new Error(`Could not find file ${name}`);
+		}
+
+      let proc;
+      const start = debounce(()=> {
+         if(proc){
+            proc.kill();
+         }
+         console.log('>>>>>>>>>STARTING PROCESS>>>>>>>>>')
+         proc = spawn('node', [name], { stdio: 'inherit' })
+      }, 200)
+		chokidar
+			.watch('.')
+			.on('add', start)
+			.on('change', start)
+			.on('unlink', start);
+	});
+
+prog.parse(process.argv);
---
+++ watchit/test.js
@@ -0,0 +1,6 @@
+const poo = setInterval(()=>console.log("bye there"), 1000)
+
+
+//asdasdasd
+//asdasdasdasd
+//asdasd
